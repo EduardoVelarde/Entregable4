c08d9607102d95abe1b1badc70d1c6daa49b53b8
EduardoVelarde <55457194+EduardoVelarde@users.noreply.github.com>
2021-11-12 01:59:13 -0600

    updates
    updates

--- handlebears_project/index.js
+++ handlebears_project/index.js
@@ -17,7 +17,10 @@ let arr=[
         url:"https://www.officedepot.com.mx/medias/45607.jpg-1200ftw?context=bWFzdGVyfHJvb3R8MjIzOTIwfGltYWdlL2pwZWd8aDZjL2gwMC85NzM0OTg1Mjg1NjYyLmpwZ3w0Y2M5MDk3MWIyYmQ2ZTFiNTAzNzJkZDY2Y2RmYTFlMzA2MDBmNzBiZmI4YTQxZjY4NjA5MjAyM2I1MWY3ZDBm"
     }
 ]
-
+//Array users
+let msn= []
+//middlewares
+app.use(express.static(__dirname+"/public"))
 app.set("views","./views")
 app.set("view engine","hbs")
 app.engine("hbs", 
@@ -28,8 +31,24 @@ app.engine("hbs",
         partialsDir:__dirname+"/views/partials"
     })
 )
+//server
+const http=require('http')
+const server=http.createServer(app)
+ // sockt IO
+const {Server}=require('socket.io');
+const io=new Server(server)
+io.on("connection",(socket)=>{
+    socket.on("msn_client",(data)=>{
+        msn.push(data)
+        io.sockets.emit("msn_send",msn)
+        //socket.emit("msn_send",msn)
+    })
+})
+// socket IO configutation
+server.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`)
+})
 
- 
 app.get("/products",(req,res)=>{
     res.render("main",{data:arr})
 })
@@ -37,7 +56,4 @@ app.post("/products",(req,res)=>{
     console.log(req.body)
     arr.push(req.body)
     res.redirect("/products")
-})
-app.listen(PORT,()=>{
-    console.log(`Listening on port ${PORT}`)
 })
